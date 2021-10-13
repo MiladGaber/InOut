c095cb771e529bfef28f4b90ca5a3aa338bf56c1
MiladGaber <milad.gaber00@gmail.com>
2021-10-13 18:33:16 +0200

    Add project files.

---
+++ InOut/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace InOut.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
